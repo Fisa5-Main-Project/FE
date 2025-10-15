cdf3e8af0391edc8017260a07125c447f6143bf4
Lee Jong Hyuck <101847527+Jhcki222@users.noreply.github.com>
2025-10-15 13:59:21 +0900

    Docs : 템플릿 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function LandingPage() {
         <main className="flex h-screen w-full flex-col bg-white">
             <div className="flex flex-1 flex-col items-center justify-center">
                 <Image src="/assets/logo/logo.png" alt="WON 로고" width={120} height={120} priority />
-                <h1 className="mt-4 font-heading text-2xl text-primary">우리은행</h1>
+                <h1 className="mt-4 font-heading text-2xl font-bold text-primary">우리은행</h1>
             </div>
         </main>
     );
