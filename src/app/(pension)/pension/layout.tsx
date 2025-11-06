b12c2cb0935ac30c68f881d0d3dd862764782906
Jay Youngjae <jea71882@naver.com>
2025-11-06 17:44:05 +0900

    Feat: Pension 레이아웃, MyDataProvider 적용

---
+++ src/app/(pension)/pension/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { MyDataProvider } from "@/context/MyDataContext";
+
+/**
+ * Pension 라우트 그룹의 레이아웃
+ */
+
+export default function PensionLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <MyDataProvider>
+      <main className="page-container flex flex-col min-h-screen bg-gradient-to-b from-white via-[#CCE1FF] to-[#E0EDFF]">
+        {children}
+      </main>
+    </MyDataProvider>
+  );
+}
