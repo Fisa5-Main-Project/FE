04b77affbf4bba99190dc0aeed4d5cbb0eada86e
OJiyeong <owldud215@gmail.com>
2025-11-07 11:51:29 +0900

    Refactor: 추가 정보 입력 안내 페이지 구조 변경 및 버튼 하단 고정

--- src/components/mydata/steps/AdditionalInfoStep.tsx
+++ src/components/mydata/steps/AdditionalInfoStep.tsx
@@ -8,12 +8,12 @@ import Button from '@/components/common/Button';
  * - 라우팅: 버튼 클릭 시 useRouter를 통해 다음 페이지로 이동합니다.
  */
 const AdditionalInfoStep = () => {
-  const router = useRouter();
 
   return (
-    <div className="flex flex-col h-full justify-between">
-      <div className="mt-[81px]">
-        <h1 className="text-[28px] font-medium leading-relaxed text-secondary">
+    <div className="w-full">
+
+      <div className="mt-[4.875rem]">
+        <h1 className="text-[1.75rem] font-medium leading-relaxed text-secondary">
           정확한 서비스 제공을 위해
           <br />
           마이데이터로 연동되지 않는
@@ -23,11 +23,7 @@ const AdditionalInfoStep = () => {
           </strong>
           를 입력해주세요.</h1>
       </div>
-      <div className="w-full">
-        <Button onClick={() => router.push('/mydata/assets')} >
-          정보 추가로 입력하기
-        </Button>
-      </div>
+      
     </div>
   );
 };
