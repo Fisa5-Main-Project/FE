7b052212e26da86386ffb43840b5942302b4e544
OJiyeong <owldud215@gmail.com>
2025-11-07 11:23:23 +0900

    Refactor: 로딩 페이지 구조 변경

--- src/app/(mydata)/mydata/loading/page.tsx
+++ src/app/(mydata)/mydata/loading/page.tsx
@@ -1,10 +1,22 @@
 'use client';
 
 import LoadingStep from '@/components/mydata/steps/LoadingStep';
+import { useRouter } from 'next/navigation';
 
 /**
  * 마이데이터 연동 - 로딩 페이지
  */
 export default function LoadingPage() {
-  return <LoadingStep />;
+  const router = useRouter();
+
+  // 로딩 완료 후 다음 페이지로 이동
+  const handleLoadingComplete = () => {
+    //router.push('/mydata/complete');
+  };
+
+  return (
+    <div className="h-full px-[2.8125rem] pb-[3.65rem]">
+      <LoadingStep onComplete={handleLoadingComplete}/>
+    </div>
+);
 }
