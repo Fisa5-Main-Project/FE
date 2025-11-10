19aa51729e34f543c833df412aac77a81f28e244
Lee Jong Hyuck <101847527+Jhcki222@users.noreply.github.com>
2025-11-10 16:03:52 +0900

    Feat : 자산관리 페이지 구현

--- src/components/asset/AssetStartPageClient.tsx
+++ src/components/asset/AssetStartPageClient.tsx
@@ -4,12 +4,7 @@ import Button from '@/components/common/Button';
 import { PageActions } from '@/components/common/Page';
 import { useAssetRouter } from '@/hooks/asset/useAssetRouter';
 
-interface AssetStartPageClientProps {
-    // If any static content needs to be passed from the server component
-    // For now, the button is the only interactive part
-}
-
-export function AssetStartPageClient(props: AssetStartPageClientProps) {
+export function AssetStartPageClient() {
     const { goTo } = useAssetRouter();
 
     return (
