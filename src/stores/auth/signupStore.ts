118f271b4fafef6ba451faf1567101449ff8c4a7
uxxin <dbwlsdid1114@naver.com>
2025-11-13 12:30:19 +0900

    feat: 회원가입 2단계-약관 정보 가공 후 스토어에 저장

--- src/stores/auth/signupStore.ts
+++ src/stores/auth/signupStore.ts
@@ -1,21 +1,33 @@
 import { create } from "zustand";
+import type { TermAgreement } from "@/types/signup";
 
 // 최종 제출에 필요한 정보들
 interface SignupData {
   verificationId?: string; // [1단계 본인확인] 정보
+  termAgreements: TermAgreement[]; // [2단계 약관] 정보
 }
 
 interface SignupStore {
   data: SignupData;
   setVerificationId: (id: string) => void;
+  setTermAgreements: (agreements: TermAgreement[]) => void;
   clearData: () => void;
 }
 
+const initialState: SignupData = {
+  verificationId: undefined,
+  termAgreements: [],
+};
+
 export const useSignupStore = create<SignupStore>((set) => ({
-  data: {},
+  data: initialState,
   setVerificationId: (id) =>
     set((state) => ({
       data: { ...state.data, verificationId: id },
     })),
-  clearData: () => set({ data: {} }),
+  setTermAgreements: (agreements) =>
+    set((state) => ({
+      data: { ...state.data, termAgreements: agreements },
+    })),
+  clearData: () => set({ data: initialState }),
 }));
